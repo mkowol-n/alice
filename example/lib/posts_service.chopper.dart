510c297d6fab6420d9a8f4fded8047201e46bc9a
Mateusz Kowol <mateusz.kowol@euvic.pl>
2023-05-24 17:05:58 +0200

    Fix

--- example/lib/posts_service.chopper.dart
+++ example/lib/posts_service.chopper.dart
@@ -19,23 +19,26 @@ class _$PostsService extends PostsService {
   @override
   Future<Response<dynamic>> getPost(String id) {
     final $url = 'https://jsonplaceholder.typicode.com/posts/$id';
-    final $request = Request('GET', $url, client.baseUrl);
+    final uri = Uri.parse($url);
+    final $request = Request('GET', uri, client.baseUrl);
     return client.send<dynamic, dynamic>($request);
   }
 
   @override
   Future<Response<dynamic>> postPost(String? body) {
     final $url = 'https://jsonplaceholder.typicode.com/posts/';
+    final uri = Uri.parse($url);
     final $body = body;
-    final $request = Request('POST', $url, client.baseUrl, body: $body);
+    final $request = Request('POST', uri, client.baseUrl, body: $body);
     return client.send<dynamic, dynamic>($request);
   }
 
   @override
   Future<Response<dynamic>> putPost(String id, String? body) {
     final $url = 'https://jsonplaceholder.typicode.com/posts/$id';
+    final uri = Uri.parse($url);
     final $body = body;
-    final $request = Request('PUT', $url, client.baseUrl, body: $body);
+    final $request = Request('PUT', uri, client.baseUrl, body: $body);
     return client.send<dynamic, dynamic>($request);
   }
 }
